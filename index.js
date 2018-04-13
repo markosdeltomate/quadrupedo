18a721a2902ff01952dfdf2975cd1636f805c162
Marcos Tomatti <mtomatti@elementum.com>
2018-04-13 20:12:14 -0300

    Prueba Basica de 2 motores conectados directamente al arduino

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const five = require('johnny-five');
 const board = new five.Board();
 
 board.on('ready', () => {
-  const led = new five.Led(13);
-  board.repl.inject({ led });
+  const motorA = new five.Servo({ pin: 8, startAt: 90 });
+  const motorB = new five.Servo({ pin: 9, startAt: 0 });
+  const motores = new five.Servos([motorA, motorB]);
+
+
+  board.repl.inject({ motorA, motorB, motores });
 });
+
