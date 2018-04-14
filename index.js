6d72f5fc540d665c878c36665e24572b5a3d8e91
Marcos Tomatti <mtomatti@elementum.com>
2018-04-14 10:40:43 -0300

    Agregando comentarios

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ board.on('ready', () => {
   const motorB = new five.Servo({ pin: 9, startAt: 0 });
   const motores = new five.Servos([motorA, motorB]);
 
+  /*
+   * Servo Motores
+   * Los servos pueden incluir en su configuracion un controller, como el PCA9685 con su Address,
+   * tambien pueden tener posicion de inicio, rango de angulos y home position. Ademas aceptan un
+   * parametro "inverted" para identificar si rotan en sentido del reloj o el inverso.
+   */
 
   board.repl.inject({ motorA, motorB, motores });
 });
