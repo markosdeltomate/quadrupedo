6c0ca86a06f0500c2e5a9fdf49f60a5ced70ea4a
Marcos Tomatti <mtomatti@elementum.com>
2018-04-13 21:46:21 -0300

    Ahora camina

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const five = require('johnny-five');
 const EtherPort = require('etherport');
+const temporal = require('temporal');
+
 const board = new five.Board({ port: new EtherPort(3030), timeout: 30000 });
 
 board.on('ready', () => {
   const quad = {
     status: 'sleep'
   };
+  const altura = 20;
   const centro = 90;
   const zancada = 1;
   const angulos = {
@@ -54,7 +57,7 @@ board.on('ready', () => {
   const animacionPiernas = new five.Animation(quad.piernas);
   const animacionFemures = new five.Animation(quad.femures);
 
-  quad.sleep = function sleep() {
+  quad.abajo = function abajo() {
     animacionFemures.enqueue({
       duration: 1000,
       fps: 100,
@@ -72,7 +75,169 @@ board.on('ready', () => {
     });
   };
 
-  quad.stand = () => {
+  quad.caminar = function caminar(dir) {
+    const a = dir === 'rev' ? 0 : 2;
+    const b = dir === 'rev' ? 2 : 0;
+
+    animacionPiernas.enqueue({
+      duration: 1500,
+      cuePoints: [0, 0.25, 0.5, 0.625, 0.75, 0.875, 1.0],
+      loop: true,
+      loopback: 0.5,
+      fps: 100,
+      onstop: function onstop() {
+        quad.firme();
+      },
+      oncomplete: function oncomplete() {},
+      keyFrames: [
+        /* d1c, r1f */
+        [
+          null,
+          null,
+          { degrees: angulos.frente.cadera[a] },
+          { degrees: angulos.frente.cadera[1] },
+          { degrees: angulos.frente.cadera[b] },
+          null,
+          { degrees: angulos.frente.cadera[a] }
+        ],
+        [
+          null,
+          { step: altura, easing: easeOut },
+          { degrees: angulos.frente.femur[a], easing: easeIn },
+          { degrees: angulos.frente.femur[1] },
+          { degrees: angulos.frente.femur[b] },
+          { step: altura, easing: easeOut },
+          { degrees: angulos.frente.femur[a], easing: easeIn }
+        ],
+
+        /* i1c, l1f */
+        [
+          null,
+          null,
+          { degrees: angulos.frente.cadera[b] },
+          null,
+          { degrees: angulos.frente.cadera[a] },
+          { degrees: angulos.frente.cadera[1] },
+          { degrees: angulos.frente.cadera[b] }
+        ],
+        [
+          null,
+          null,
+          { degrees: angulos.frente.femur[b] },
+          { step: altura, easing: easeOut },
+          { degrees: angulos.frente.femur[a], easing: easeIn },
+          { degrees: angulos.frente.femur[1] },
+          { degrees: angulos.frente.femur[b] }
+        ],
+
+        /* d2c, r2f */
+        [
+          null,
+          null,
+          { degrees: angulos.atras.cadera[b] },
+          null,
+          { degrees: angulos.atras.cadera[a] },
+          { degrees: angulos.atras.cadera[1] },
+          { degrees: angulos.atras.cadera[b] }
+        ],
+        [
+          null,
+          null,
+          { degrees: angulos.atras.femur[b] },
+          { step: altura, easing: easeOut },
+          { degrees: angulos.atras.femur[a], easing: easeIn },
+          { degrees: angulos.atras.femur[1] },
+          { degrees: angulos.atras.femur[b] }
+        ],
+
+        /* i2c, l2f */
+        [
+          null,
+          null,
+          { degrees: angulos.atras.cadera[a] },
+          { degrees: angulos.atras.cadera[1] },
+          { degrees: angulos.atras.cadera[b] },
+          null,
+          { degrees: angulos.atras.cadera[a] }
+        ],
+        [
+          null,
+          { step: altura, easing: easeOut },
+          { degrees: angulos.atras.femur[a], easing: easeIn },
+          { degrees: angulos.atras.femur[1] },
+          { degrees: angulos.atras.femur[b] },
+          { step: altura, easing: easeOut },
+          { degrees: angulos.atras.femur[a], easing: easeIn }
+        ]
+      ]
+    });
+  };
+
+
+  quad.firme = function firme() {
+    const work = [
+      {
+        name: 'd1',
+        offset: 0,
+        home: angulos.frente.femur[1],
+        chome: angulos.frente.cadera[1]
+      },
+      {
+        name: 'd2',
+        offset: 0,
+        home: angulos.atras.femur[1],
+        chome: angulos.frente.cadera[1]
+      },
+      {
+        name: 'i1',
+        offset: 0,
+        home: angulos.frente.femur[1],
+        chome: angulos.frente.cadera[1]
+      },
+      {
+        name: 'i2',
+        offset: 0,
+        home: angulos.atras.femur[1],
+        chome: angulos.frente.cadera[1]
+      }
+    ];
+
+    work.forEach((pierna, i) => {
+      work[i].offset = Math.abs(quad[`${pierna.name}f`].last.reqDegrees - pierna.home);
+    });
+
+    const grouped = (work[1].offset > work[3].offset) ?
+      [[0, 2], [1, 3]] :
+      [[1, 3], [0, 2]];
+
+    grouped.forEach((group, i) => {
+      group.forEach((leg) => {
+        temporal.queue([
+          {
+            delay: 250 * i,
+            task: () => {
+              quad[`${work[leg].name}f`].to(work[leg].home + altura);
+            }
+          },
+          {
+            delay: 50,
+            task: () => {
+              quad[`${work[leg].name}c`].to(work[leg].chome);
+            }
+          },
+          {
+            delay: 50,
+            task: () => {
+              quad[`${work[leg].name}f`].to(work[leg].home);
+            }
+          }
+        ]);
+      });
+    });
+    quad.state = 'stand';
+  };
+
+  quad.arriba = () => {
     quad.cadera.to(60);
     quad.femures.to(30);
   };
